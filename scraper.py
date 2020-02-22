c3965a731b3a9c7296d75d02a868ce3689ffecd0
Paul Lambert <paul.lambert@linux.com>
2020-02-22 10:11:46 -0600

    welp, I've got the live stats data in JSON from each game..

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
-from bs4 import BeautifulSoup
+import json
 
 def calculate_num_games():
     """
@@ -10,7 +10,7 @@ def calculate_num_games():
     """
     return 8
 
-def simple_get(url):
+def fetch_pbp(url):
     """
     Attempts to get the content at `url' by making an HTTP GET request.
     If the content-type of response is HTML/XML return content; otherwise
@@ -20,7 +20,11 @@ def simple_get(url):
     try:
         with closing(get(url, stream=True)) as resp:
             if is_good_response(resp):
-                return resp.content
+                resp = resp.text
+                start = resp.find('playList = ') + len('playList = ')
+                end = resp.find('</script>', start)
+                live_stats = resp[start:end]
+                return live_stats
             else:
                 return None
 
@@ -44,10 +48,11 @@ def log_error(e):
 
 def main():
     num_games = calculate_num_games()
+
     for match in range(1, num_games+1):
         print("We're getting the data for Match %d" % (match))
         url = "https://stats.xfl.com/" + str(match)
-        data = simple_get(url)
+        data = fetch_pbp(url)
         print(data)
 
 if __name__ == '__main__':
